9e61f14388a9ee133df738054fb68f7f07fccf24
Ofir <t0n2b@ugrad.cs.ubc.ca>
2018-10-16 12:06:20 -0700

    some errors were fixed

--- pseudo.hs
+++ pseudo.hs
@@ -3,8 +3,11 @@
 import Control.Monad (replicateM)
 import Data.List
 import Cross
+import Data.Maybe
+import System.Random
 
 type Population = [Chromosome]
+type Fit = (Chromosome -> Double)
 
 ----- Client API types Definition
 
@@ -19,20 +22,20 @@ mutate::Chromosome -> Double -> Chromosome
 -- data result = (Chromosome, population) -- best Chromosome, whole population
 -}
 
-genetic _ _ _ _ _ 0 population = do return population !! indexOfBest population
+genetic fit _ _ _ _ 0 population = do return population !! (indexOfBest fit population)
 genetic fit cross mutate pc pm maxIterations population =
     do
         rndIndex <- replicateM (2 * length population) $ randomRIO (0, (length population - 1) :: Int)
         rndCrossProbs <- replicateM (length population / 2) $ randomRIO (0, 1 :: Double)
         rndMutProb <- replicateM (length population) $ randomRIO (0, 1 :: Double)
 
-        let best = population !! indexOfBest population
+        let best = population !! (indexOfBest fit population)
         let selectPop = select fit population rndIndex 0
         let crossPop = crossAll cross pc rndCrossProbs selectPop
         let mutatePop = [if p < pm then mutate c else c | (c, p) <- zip population rndMutProb]
         let replacePop = replacement fit mutatePop best
 
-        return genetic fit cross mutate pc pm maxIterations-1 replacePop
+        return genetic fit cross mutate pc pm (maxIterations-1) replacePop
 
 -- typedef for crossAll
 type Crossfunc = Chromosome -> Chromosome -> (Chromosome, Chromosome)
@@ -63,10 +66,10 @@ replaceNth (x:xs) newVal n
     | otherwise = x:replaceNth xs newVal (n-1)
 
 -- Select a Population using binary tournament
-select::fit -> Population -> [Int] -> Int -> Population
-select fit population (i1:(i2:t)) i =
+select:: Fit -> Population -> [Int] -> Int -> Population
+select fit population (i1:(i2:t)) i
     | i >= length population = population
-    | otherwise = select fit (replaceNth population new i) t i+1
+    | otherwise = select fit (replaceNth population new i) t (i+1)
     where
         new = binaryTournament fit population i1 i2
 
@@ -77,11 +80,11 @@ replacement fit population best = replaceNth population best worst
         worst = indexOfWorst fit population
 
 -- get the index of the worst solution in a population
-indexOfWorst fit lst = elemIndex (minimum fittedLst) fittedLst 
+indexOfWorst fit lst = fromJust (elemIndex (minimum fittedLst) fittedLst) 
     where
         fittedLst = map fit lst
 
 --get the index of the best solution in a population
-indexOfBest fit lst = elemIndex (maximum fittedLst) fittedLst 
+indexOfBest fit lst = fromJust (elemIndex (maximum fittedLst) fittedLst) 
     where
         fittedLst = map fit lst
