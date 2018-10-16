d9529ff83927d08dbb8217761f0b39bef6f624f5
Junsu Shin <junsushin4546@gmail.com>
2018-10-16 00:11:36 -0700

    imported Cross.hs and removed unnecessary duplicate declarations. Also modified Ofir's crossAll to work together with module Cross

--- pseudo.hs
+++ pseudo.hs
@@ -1,22 +1,22 @@
 -- CPSC 312 - 2018 - Genetic Algorithm Library
 
 import Data.List
+import Cross
+
+type Population = [Chromosome]
 
 ----- Client API types Definition
--- Fitness function. From a chromosome it must return a Num regarding how good this chromosome is
-fit::chromosome -> Num
 
--- cross two chromosomes to make a pair of children
-cross::chromosome -> chromosome -> (chromosome, chromosome)
+-- TBD
+{-
+-- Fitness function. From a Chromosome it must return a Num regarding how good this Chromosome is
+fit::Chromosome -> Num
 
--- mutate a single chromosome
-mutate::chromosome -> pm -> chromosome
+-- mutate a single Chromosome according to pm(mutation probability)
+mutate::Chromosome -> Double -> Chromosome
 
-population::[chromosome]
-chromosome::[a]
-
--- data result = (chromosome, population) -- best chromosome, whole population
--- data chromosome = [a]
+-- data result = (Chromosome, population) -- best Chromosome, whole population
+-}
 
 genetic _ _ _ _ _ 0 population = population !! indexOfBest population
 genetic fit cross mutate pc pm maxIterations population = genetic fit cross mutate pc pm maxIterations-1 replacePop best (mutatePop pm (crossPop pc (selectPop population)))
@@ -27,9 +27,22 @@ genetic fit cross mutate pc pm maxIterations population = genetic fit cross muta
         mutatePop pop = [mutate c | c <- pop | shouldApplyMut pm] -- generate random number in shouldApplyMut
         replacePop b pop = replacement fit pop b
 
--- population::[[]]
+-- typedef for crossAll
+type Crossfunc = Chromosome -> Chromosome -> (Chromosome, Chromosome)
+type Crossprob = Double
+
+-- cross the population to generate new population
+-- We will cross contiguous parents (i, i+1). If a random double in [0,1] is less than pc,
+-- replace i and i+1 by the pair of new chromosomes returned from cross function
+crossAll::Crossfunc -> Crossprob -> [Double] -> Population -> Population
+crossAll _ _ _ [] = []
+crossAll cross pc (p:t) (a:(b:pop)) = if (p < pc) then sa : (sb : crossedPop) else a : (b : crossedPop)
+    where
+        (sa, sb) = cross a b
+        crossedPop = crossAll cross pc t pop
+
 -- apply it n times (n = number of population) maybe fold or map (a is dummy for map use)
-binaryTournament::Integral a => fit -> population -> a -> a -> chromosome  
+binaryTournament::Integral a => fit -> Population -> a -> a -> Chromosome  
 binaryTournament fit population i1 i2 = if (fit s1) > (fit s2) then s1 else s2
     where
         s1 = population!!i1
@@ -43,13 +56,13 @@ replaceNth (x:xs) newVal n
     | otherwise = x:replaceNth xs newVal (n-1)
 
 -- Select a Population using binary tournament
-select::fit -> population -> population
+select::fit -> Population -> Population
 select fit population = map (replaceNth population new) [0..(length population)]
     where
         new = binaryTournament fit population
 
 -- make sure that the best genes are selected into the population (elitism)
-replacement::fit -> population -> population
+replacement::fit -> Population -> Population
 replacement fit population best = replaceNth population best worst
     where
         worst = indexOfWorst fit population
@@ -66,14 +79,3 @@ indexOfBest fit lst = elemIndex (maximum fittedLst) fittedLst
 
 -- Return True if generated number is below pm.
 shouldApplyMut pm p = if p < pm then True else False
-
--- cross the population to generate new population
--- We will cross contiguous parents (i, i+1). If a random double in [0,1] is less than pc,
--- replace i and i+1 by the pair of new chromosomes returned from cross function
-crossALL:: (Integral a) => cross -> pc -> population -> population
-
-crossAll _ [] = []
-crossAll pc (a:b:pop) p = if (p < pc) then sa : sb : crossedPop else a : b : crossedPop
-    where
-        (sa, sb) = cross a b
-        crossedPop = crossAll pc pop
